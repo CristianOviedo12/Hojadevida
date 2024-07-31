e0f44ce9ba54def342279f2a9d8aec483efcb572
crisoviedo <coviedop4@gmail.com>
2024-07-31 18:02:12 -0500

    Actualizacion de la COMPONENTE APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 
-import './App.css'
+import * as React from 'react';
+import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
 
-function App() {
+import './App.css';
 
-  return (
+const App = () => {
+  return(
     <>
-      <h1>Cristian Maurcio Oviedo Pacheco</h1>
+      <Container className='form-container'>
+        <Box className='form-box'>
+          <Typography>Registro de usuario</Typography>
+        </Box>
+        <form>
+          <Grid className='form-grid'>
+            <Grid>
+              <TextField fullWidth id='username' label='nombre de usuario' variant='outlined'></TextField>
+            </Grid>
+            <Grid>
+              <TextField fullWidth id='email' label='correo electronico' variant='outlined'></TextField>
+            </Grid>
+            <Grid>
+              <Button type='submit' id='submit-btn' variant='outlined' fullWidth>
+                Registrar
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
     </>
   )
 }
 
-export default App
+export default App;
+
+
+
