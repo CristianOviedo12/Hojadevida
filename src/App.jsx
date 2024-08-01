f51533da5912749ca1523ec5688b72a3eec515b0
crisoviedo <coviedop4@gmail.com>
2024-08-01 15:31:21 -0500

    Se termina el formulario de registro con sus respectivos css, aunque se usaron sx en los componentes directamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,74 @@
-
 import * as React from 'react';
 import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
 
-import './App.css';
-
 const App = () => {
-  return(
-    <>
-      <Container className='form-container'>
-        <Box className='form-box'>
-          <Typography>Registro de usuario</Typography>
-        </Box>
-        <form>
-          <Grid className='form-grid'>
-            <Grid>
-              <TextField fullWidth id='username' label='nombre de usuario' variant='outlined'></TextField>
+  return (
+    <Container
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Box
+        sx={{
+          width: '30%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '16px',
+        }}
+      >
+        <Typography variant="h6" gutterBottom sx={{ marginBottom:'20px'}}>
+          Registro de usuario
+        </Typography>
+        <form
+          sx={{
+            width: '100%'
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="username"
+                name="username"
+                label="Nombre de usuario"
+                variant="outlined"
+              />
             </Grid>
-            <Grid>
-              <TextField fullWidth id='email' label='correo electronico' variant='outlined'></TextField>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Correo electrónico"
+                variant="outlined"
+              />
             </Grid>
-            <Grid>
-              <Button type='submit' id='submit-btn' variant='outlined' fullWidth>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                id="submit-btn"
+                variant="outlined"
+                fullWidth
+                sx={{color: 'black', borderColor:'black','&:hover':{color:'#1976d2',transition:'1s'}}}
+              >
                 Registrar
               </Button>
             </Grid>
           </Grid>
         </form>
-      </Container>
-    </>
-  )
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
+
 
 
 
