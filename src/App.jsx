dd3fae6278de39e00c2325e441c8320d1fcec458
crisoviedo <coviedop4@gmail.com>
2024-09-05 11:25:15 -0500

    solucion de styles-components y de vite config js

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link as RouterLink } from 'react-router-dom';
 import Home from './pages/Home';
-import Registro from "c:/MyHoja/proyectocm/src/pages/Registro/Registro";
+import Registro from './pages/registro/Registro';  // Usa una ruta relativa
 import Iniciosesion from './pages/iniciosesion/Iniciosesion';
 import Hojadevida from './pages/paginas/Hojadevida';
 import RutaProtegida from './pages/RutaProtegida';
 import styled from 'styled-components';
 
-const App = () => {
-
-  const [isAuthenticated, setIsAuthenticated ] = React.useState(false);
-
-  React.useEffect(()=>{
-    const authStatus = localStorage.getItem('isAuthenticated');
-    if (authStatus === 'true') {
-      setIsAuthenticated(true);
-    }
-  },[]);
-
-  const StyledRouterLink = styled(RouterLink)`
+const StyledRouterLink = styled(RouterLink)`
   color: white;
   margin-right: 15px;
   text-decoration: none;
@@ -30,34 +19,43 @@ const App = () => {
     transition: background-color 0.8s ease;
   }
 `;
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  React.useEffect(() => {
+    const authStatus = localStorage.getItem('isAuthenticated');
+    if (authStatus === 'true') {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
     <Router>
-
       {isAuthenticated ? (
         <Hojadevida />
-      ) :  (
+      ) : (
         <>
-        <div style={{background:'black',color:'white', height:'10vh',display:'flex',alignItems:'center'}}>
-        <nav>
-        <div>
-          <StyledRouterLink to='/registro'>Registrese</StyledRouterLink>
-          <StyledRouterLink to='/sesion'>Iniciar Sesión</StyledRouterLink>
-        </div>
-        </nav>
-      </div>
-      <div style={{background:'black',color:'white', height:'80vh',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/registro' element={<Registro />} />
-          <Route path='/sesion' element={<Iniciosesion setIsAuthenticated={setIsAuthenticated}/>}/>
-          <Route path='/secret' element={<RutaProtegida component={Hojadevida} isAuthenticated={isAuthenticated}  />}/>
-          
-        </Routes>
-      </div>
-      <div style={{background:'black',color:'white', height:'10vh',display:'flex',alignItems:'center'}}>
-        <i style={{fontSize:'12px'}}>Cristian Mauricio Oviedo Pacheco</i>
-        <i style={{fontSize:'12px'}}>-coviedop4@gmail.com</i>
-      </div>
+          <div style={{ background: 'black', color: 'white', height: '10vh', display: 'flex', alignItems: 'center' }}>
+            <nav>
+              <div>
+                <StyledRouterLink to='/registro'>Registrese</StyledRouterLink>
+                <StyledRouterLink to='/sesion'>Iniciar Sesión</StyledRouterLink>
+              </div>
+            </nav>
+          </div>
+          <div style={{ background: 'black', color: 'white', height: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/registro' element={<Registro />} />
+              <Route path='/sesion' element={<Iniciosesion setIsAuthenticated={setIsAuthenticated} />} />
+              <Route path='/secret' element={<RutaProtegida component={Hojadevida} isAuthenticated={isAuthenticated} />} />
+            </Routes>
+          </div>
+          <div style={{ background: 'black', color: 'white', height: '10vh', display: 'flex', alignItems: 'center' }}>
+            <i style={{ fontSize: '12px' }}>Cristian Mauricio Oviedo Pacheco</i>
+            <i style={{ fontSize: '12px' }}>-coviedop4@gmail.com</i>
+          </div>
         </>
       )}
     </Router>
@@ -65,7 +63,3 @@ const App = () => {
 }
 
 export default App;
-
-
-
-
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        // Añade aquí el módulo que está causando el problema
+        'nombre-del-modulo'
+      ],
+    },
+  },
 })
