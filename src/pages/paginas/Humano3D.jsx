2a0bd1de89cef3ade71ef3f7754b502522ac380f
crisoviedo <coviedop4@gmail.com>
2024-09-11 15:02:14 -0500

    Configuracion de frontend

--- src/pages/paginas/Humano3D.jsx
+++ src/pages/paginas/Humano3D.jsx
@@ -3,30 +3,38 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF, PerspectiveCamera } from '@react-three/drei';
 import './Humano3D.css'; // Incluye el archivo CSS
 
-const Humano3D = () => {
-  const { scene } = useGLTF('/ciber/scene.gltf');
+const Model = (props) => {
+  // Usa useGLTF para cargar el modelo
+  const { scene } = useGLTF('./ciber/scene.gltf'); // Cambia la ruta a tu modelo GLB o GLTF
 
+  return (
+    <primitive 
+      object={scene} 
+      scale={[3, 3, 3]} 
+      position={[0, -5, 0]} 
+      {...props} 
+    />
+  );
+};
+
+const Humano3D = () => {
   return (
     <div className="canvas-container">
       <Canvas>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} />
-        <primitive 
-          object={scene} 
-          scale={[3, 3, 3]} 
-          position={[0, -5, 0]}  // Ajusta esta posición para alinear el modelo con la base
-        />
+        <Model /> {/* Renderiza el modelo 3D */}
         <PerspectiveCamera 
           makeDefault 
-          position={[5, 5, 5]} // Ajusta la posición de la cámara para encuadrar el modelo
-          fov={75} // Ajusta el campo de visión
+          position={[-5, -5, -5]} 
+          fov={75} 
         />
         <OrbitControls 
           enableZoom={true} 
           enablePan={true} 
           enableRotate={true} 
           maxPolarAngle={Math.PI / 2} 
-          minPolarAngle={Math.PI / 2}
+          minPolarAngle={0} 
           target={[0, 0, 0]} 
         />
       </Canvas>
